60eec8168c127a1d18894160d9afa5e0abb9dfa0
Dave Luhman <dave@ado.software>
2023-10-17 20:41:19 -0500

    added hbs config added main layout added 404 error partial

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
+import express, { static } from 'express';
+import { config } from 'dotenv';
+import morgan from 'morgan';
 
-dotenv.config({ path: './config/config.env' }); //dotenv file
+config({ path: './config/config.env' }); //dotenv file
 const PORT = process.env.PORT || 5555
 const app = express();
 
+//handlebars setup
+const hbs = create({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    partialsDir: ['./src/views/partials',],
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true
+    }
+})
+app.engine('.hbs', hbs.engine)
+app.set('view engine', '.hbs')
+app.set('views', './src/views')
+
+
 // Logging
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 };
-app.use('/', express.static('public'));
+app.use(express.static('./src/public')) // Serve Static Files
+// catch 404 and forward to error handler
+app.use((_req, res) => {
+    res.status(404).render('error/404', { layout: 'public', title: '404' })
+})
 app.listen(PORT), console.log(`Server running on ${process.env.NODE_ENV} on port ${PORT}`);
